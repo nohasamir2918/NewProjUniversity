9092309169587fe85ebddf55b1c9d89af34b7992
Administrator <Administrator@DESKTOP-ECKFELO>
2026-01-24 21:47:01 +0200

    aya

---
+++ Presentation/ASPNET/BackEnd/Controllers/Write-offController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET.BackEnd.Controllers
+{
+    public class Write_offController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
