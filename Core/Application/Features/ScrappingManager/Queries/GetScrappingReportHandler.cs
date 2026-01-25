acb83706a6fdb5b5c03528e0aa4f920bd42e6751
noha samir <nohasamir912@gmai.com>
2026-01-25 21:53:42 +0200

    noha

--- Core/Application/Features/ScrappingManager/Queries/GetScrappingReportHandler.cs
+++ Core/Application/Features/ScrappingManager/Queries/GetScrappingReportHandler.cs
@@ -30,33 +30,33 @@ namespace Application.Features.ScrappingManager.Queries
      CancellationToken cancellationToken)
         {
             var query =
-                from s in _context.Scrapping.AsNoTracking()
-                join it in _context.InventoryTransaction.AsNoTracking()
-                    on s.Id equals it.ModuleId
+                //from s in _context.Scrapping.AsNoTracking()
+                from it in _context.InventoryTransaction.AsNoTracking()
+                   // on s.Id equals it.ModuleId
                 join p in _context.Product.AsNoTracking()
                     on it.ProductId equals p.Id
                 join w in _context.Warehouse.AsNoTracking()
-                    on s.WarehouseId equals w.Id
+                    on it.WarehouseId equals w.Id
                 join u in _context.UnitMeasure.AsNoTracking()
                     on p.UnitMeasureId equals u.Id into unit
                 from u in unit.DefaultIfEmpty()
-                where it.ModuleName == "Scrapping"
-                select new { s, it, p, w, u };
+                //where it.ModuleName == "Scrapping"
+                select new {  it, p, w, u };
 
             // IsDeleted
             if (request.IsDeleted.HasValue)
-                query = query.Where(x => x.s.IsDeleted == request.IsDeleted);
+                query = query.Where(x => x.it.IsDeleted == request.IsDeleted);
 
             // Date Filter
-            if (request.FromDate.HasValue)
-                query = query.Where(x => x.s.ScrappingDate >= request.FromDate);
+            //if (request.FromDate.HasValue)
+            //    query = query.Where(x => x.s.ScrappingDate >= request.FromDate);
 
-            if (request.ToDate.HasValue)
-                query = query.Where(x => x.s.ScrappingDate <= request.ToDate);
+            //if (request.ToDate.HasValue)
+            //    query = query.Where(x => x.s.ScrappingDate <= request.ToDate);
 
             // Warehouse Filter
             if (!string.IsNullOrEmpty(request.WarehouseId))
-                query = query.Where(x => x.s.WarehouseId == request.WarehouseId);
+                query = query.Where(x => x.it.WarehouseId == request.WarehouseId);
 
             // Product Filter
             if (!string.IsNullOrEmpty(request.ProductId))
@@ -65,16 +65,16 @@ namespace Application.Features.ScrappingManager.Queries
             var entities = await query
                 .Select(x => new ScrappingReportDto
                 {
-                    ScrappingNumber = x.s.Number,
-                    ScrappingDate = x.s.ScrappingDate.Value,
+                    ModuleName = x.it.ModuleName,
+                    TransactionDate = x.it.MovementDate.Value,
                     WarehouseName = x.w.Name,
                     ProductCode = x.p.Number,
                     ProductName = x.p.Name,
                     UnitName = x.u.Name,
                     Quantity = decimal.Abs((decimal)(x.it.Movement ?? 0)),
-                    Notes = x.s.Description
+                   // Notes = x.s.Description
                 })
-                .OrderBy(x => x.ScrappingDate)
+                .OrderBy(x => x.TransactionDate)
                 .ToListAsync(cancellationToken);
 
             return new GetScrappingReportResult
